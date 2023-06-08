f7f92ba3659a51dbebe82286e1550898fe684973
SamuelePassiatore <semmisno2@protonmail.com>
2023-06-08 16:38:08 +0200

    added pizza component with index, show and delete

--- frontend/vite-project/src/components/HomePage.vue
+++ frontend/vite-project/src/components/HomePage.vue
@@ -1,13 +1,39 @@
-<script setup>
+<script>
+import PizzaComp from './PizzaComp.vue';
+import axios from 'axios';
+const BASE_API_URL = "http://localhost:8080/api/v1";
+
+export default {
+  data() {
+    return {
+      pizze: [],
+    }
+  },
+  components: {
+    PizzaComp
+  },
+  methods: {
+    getPizze() {
+      axios.get(BASE_API_URL + "/pizze")
+        .then(res => {
+
+          const pizze = res.data;
+          console.log(pizze);
+          this.pizze = pizze;
+        })
+        .catch(err => console.log(err));
+    }
+  },
+  mounted() {
+    this.getPizze();
+  }
+}
 
 </script>
 
 <template>
-  <h1>Pizzeria</h1>
+  <h1 class="text-center p-2">Pizzeria</h1>
+  <PizzaComp class="text-center p-2" v-for="pizza in pizze" :pizza="pizza" />
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
---
+++ frontend/vite-project/src/components/PizzaComp.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios';
+const BASE_API_URL = "http://localhost:8080/api/v1";
+export default {
+    data() {
+        return {
+            open: false,
+            deleted: false
+        }
+    },
+    methods: {
+        deletePizza(id) {
+            console.log("delete pizza " + id);
+            axios.delete(BASE_API_URL + "/pizze/delete/" + id)
+                .then(res => this.deleted = true)
+                .catch(err => console.log(err));
+        }
+    },
+    props: {
+        pizza: {}
+    }
+}
+</script>
+
+<template>
+    <li v-if="!deleted">
+        <span @click="open = !open">
+            {{ pizza.nome }} - {{ pizza.prezzo }}€
+        </span>
+        <button class="btn btn-small btn-danger ms-3" @click="deletePizza(pizza.id)">Elimina</button>
+        <div v-if="open">
+            {{ pizza.descrizione }}
+        </div>
+    </li>
+</template>
+
+<style></style>
